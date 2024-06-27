8921f9fa3fb44adf1fee89fce05d70f978eb1a9a
Aman <aman.verma@docquity.com>
2024-06-27 11:10:57 +0530

    Initial Commit

---
+++ WeakProxyLib/WeakProxyLib.h
@@ -0,0 +1,18 @@
+//
+//  WeakProxyLib.h
+//  WeakProxyLib
+//
+//  Created by Aman Verma on 27/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeakProxyLib.
+FOUNDATION_EXPORT double WeakProxyLibVersionNumber;
+
+//! Project version string for WeakProxyLib.
+FOUNDATION_EXPORT const unsigned char WeakProxyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeakProxyLib/PublicHeader.h>
+
+
